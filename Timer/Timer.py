1b558c2621007ef53685fee15272e9c4b15f258c
Praveen <cjpraveen@hotmail.com>
2022-10-13 20:35:22 +0530

    Update Timer.py

--- Timer/Timer.py
+++ Timer/Timer.py
@@ -1,14 +1,23 @@
+# Create a timer
+
 from time import sleep
+
+def setTimer(target_duration: int):
+    """
+    A simple timer function.
+    """
+
+    current_duration = 0
+
+    while current_duration < target_duration:
+        print(current_duration, end="\r")
+        
+        sleep(1)
+        
+        current_duration += 1
+
+    print("Your time is up!")
 
 target_duration = int(input("Enter the duration in seconds: "))
 
-current_duration = 0
-
-while current_duration < target_duration:
-    print(current_duration, end="\r")
-    
-    sleep(1)
-    
-    current_duration += 1
-
-print("Your time is up!")
+setTimer(target_duration)
