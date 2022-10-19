dbeb0e5f00283b1e205e8faa11abd8fba0100acd
pawnsuthar <116201526+pawnsuthar@users.noreply.github.com>
2022-10-19 23:51:27 +0530

    Create Music Player.py

---
+++ Python/Music Player.py
@@ -0,0 +1,19 @@
+import pygame
+import tkinter as tkr
+from tkinter.filedialog import askdirectory
+import os
+
+music_player = tkr.Tk()
+music_player.title("My Music Player")
+music_player.geometry("450x350")
+directory = askdirectory()
+os.chdir(directory)
+song_list = os.listdir()
+
+play_list = tkr.Listbox(music_player, font="Helvetica 12 bold", bg='yellow', selectmode=tkr.SINGLE)
+for item in song_list:
+    pos = 0
+    play_list.insert(pos, item)
+    pos += 1
+pygame.init()
+pygame.mixer.init()
