536384a80f30e00d1d68d94d71eff2d4d6dea3ca
Yashi Gupta <98335217+yashigupta4623@users.noreply.github.com>
2022-10-25 21:38:17 +0530

    comment.py

---
+++ comment.py
@@ -0,0 +1,17 @@
+example of single one comment  :
+#this is an example of single line comment.
+
+ example of multiple line comment :
+ 
+ ''' this 
+ is the example 
+ for multiple lines comment 
+ in python'''
+ 
+ or 
+ 
+ """this 
+ is the example 
+ for multiple lines comment 
+ in python"""
+
